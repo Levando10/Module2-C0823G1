7d1cf775eb3b09ea9693a41351fda22f72bfa144
Levando10 <102398741+Levando10@users.noreply.github.com>
2023-10-23 13:55:58 +0700

    update arraylist v2

--- week2/src/ss9/bai_tap/Implement_array_list/MyArrayList.java
+++ week2/src/ss9/bai_tap/Implement_array_list/MyArrayList.java
@@ -53,7 +53,7 @@ public class MyArrayList<E> {
   public void addFirst(E e) {
     size++;
 
-    for (int i = 0; i < size() - 1; i++) {
+    for (int i = 0; i < size(); i++) {
       E temp = (E) elements[i];
       elements[i] = e;
       e = temp;
