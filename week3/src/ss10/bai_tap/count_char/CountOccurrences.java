1f62d801d6cd9c5308dc68008131757fb22c2edf
Levando10 <102398741+Levando10@users.noreply.github.com>
2023-10-23 11:02:00 +0700

    nop bai tap count char

---
+++ week3/src/ss10/bai_tap/count_char/CountOccurrences.java
@@ -0,0 +1,23 @@
+package ss10.bai_tap.count_char;
+
+import java.util.TreeMap;
+
+public class CountOccurrences {
+
+  public static void main(String[] args) {
+    String str = "The quick dog jump dog over the cat lazy brown cat";
+    str = str.toLowerCase();
+    TreeMap<Character, Integer> strings = new TreeMap<>();
+    for (int i = 0; i < str.length(); i++) {
+      int count = 0;
+      for (int j = 0; j < str.length(); j++) {
+
+        if (str.charAt(i) == str.charAt(j)) {
+          count++;
+        }
+      }
+      strings.putIfAbsent(str.charAt(i), count);
+    }
+    System.out.println(strings);
+  }
+}
